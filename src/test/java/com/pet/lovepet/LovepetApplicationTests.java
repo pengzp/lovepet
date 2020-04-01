bd00f69e24eca2d22094096cf5325010e1d890a0
pengzhipeng <1156715571@qq.com>
2020-04-01 11:00:23 +0800

    first commit

---
+++ src/test/java/com/pet/lovepet/LovepetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.lovepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovepetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
